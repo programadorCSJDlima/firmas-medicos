fef7fb176f3787465d6766a49ca0c6dbe7227d87
programadorCSJDlima <programador.lima2021@gmail.com>
2025-08-26 15:25:38 -0500

    rutas agregadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { TutorialComponent } from './components/tutorial/tutorial.component';
+
+export const routes: Routes = [ 
+	{ path: '', component: HomeComponent }, 
+    { path: 'tutorial', component: TutorialComponent } 
+];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+   imports: [RouterOutlet,FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  
+  title = 'SIGNGEN';
+  nameDoc: string = 'Dra. Depool Angulo Ines Maria.';
+  cmpDoc: string = 'CMP.';
+  rneDoc: string = 'RNE.';
+  espDoc:string='Especialidad:'
+
+
+  /////GESTIÓN DE LA IMAGEN
+  public selectedImageUrl: string | null = null;
+  private imageFile: File | null = null;
+
+  onFileSelected(event: Event): void {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files.length > 0) {
+      this.imageFile = fileInput.files[0];
+      this.createBlobUrl();
+    }
+  }
+
+  createBlobUrl(): void {
+    if (this.selectedImageUrl) {
+      // Revocar la URL anterior para evitar fugas de memoria
+      URL.revokeObjectURL(this.selectedImageUrl);
+    }
+    if (this.imageFile) {
+      // Crear la nueva URL
+      this.selectedImageUrl = URL.createObjectURL(this.imageFile);
+    }
+  }
+
+  ngOnDestroy(): void {
+    // Revocar la URL al destruir el componente
+    if (this.selectedImageUrl) {
+      URL.revokeObjectURL(this.selectedImageUrl);
+    }
+  }
+  /////FIN DE GESTION DE LA IMAGEN
+
+
+
+
+
+  ///DESCARGA DE LA FIRMA
+  @ViewChild('contenidoParaCapturar') contenidoParaCapturar!: ElementRef;
+
+  convertirAImagen() {
+    if (this.contenidoParaCapturar) {
+      html2canvas(this.contenidoParaCapturar.nativeElement).then(canvas => {
+        // El `canvas` es la representación del elemento HTML como una imagen
+        // Ahora puedes trabajar con él para descargarlo o mostrarlo
+
+        // Opción 1: Descargar la imagen
+        const imgData = canvas.toDataURL('image/jpge');
+        const link = document.createElement('a');
+        link.download = 'firmare.jpge';
+        link.href = imgData;
+        link.click();
+
+        // Opción 2: Mostrar la imagen en otro lugar de la página
+        const imgElement = document.getElementById('imagenGenerada') as HTMLImageElement;
+        if (imgElement) {
+          imgElement.src = imgData;
+        }
+      });
+    }
+  }
+
+  //FIN DE DESCARGA DE LA FIRMA
+
 
 }
